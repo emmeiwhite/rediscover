6f73985d81f6a94a1afe111c07ccf3d3522d0a28
Imran Rather <emmeiwhite@gmail.com>
2021-07-05 18:41:18 +0530

    Created First Component NameComponent

---
+++ src/components/NameComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NameComponent() {
+    return (
+        <section>
+            This is a Name Component
+        </section>
+    )
+}
+
+export default NameComponent;
